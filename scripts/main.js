76b93e7d54b78ba2b8278a68f0137a31cab852b8
kochatkov <vlad.kochatkov@gmail.com>
2020-04-19 16:32:22 +0300

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const button = document.querySelectorAll('.carousel__btn');
+const leftBtn = button[0];
+const rightBtn = button[1];
+const ul = document.querySelector('.carousel');
+const cardWidth = ul.clientWidth;
+let transformX = 0;
+let index = 0;
+const dots = document.querySelectorAll('.carousel__dot');
+
+
+leftBtn.addEventListener('click', () => {
+  transformX += cardWidth;
+  index++;
+
+  if (transformX > 1050) {
+    transformX = 0;
+    index = 0;
+  }
+
+  ul.style.transform = `translateX(-${transformX}px)`;
+
+  changeDot();
+});
+
+rightBtn.addEventListener('click', () => {
+  transformX -= cardWidth;
+  index++;
+
+  if (transformX < -1050) {
+    transformX = 0;
+    index = 0;
+  }
+
+  ul.style.transform = `translateX(${transformX}px)`;
+  changeDot();
+});
+
+function changeDot() {
+  dots.forEach(dot => dot.classList.remove('carousel__dot_active'));
+  dots[index].classList.add('carousel__dot_active');
+}
